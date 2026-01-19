bc01d8ed98ee49eb55e1b4c2abac306f04a2126e
Elliot Anderson <elliota43@outlook.com>
2026-01-18 23:52:47 -0500

    Initial Commit

---
+++ Trace/TraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceApp.swift
+//  Trace
+//
+//  Created by Elliot Anderson on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct TraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
