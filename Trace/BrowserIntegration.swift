bbbaa975fe0850b168ebbb79a4cb4fababb1d11d
Elliot Anderson <elliota43@outlook.com>
2026-01-19 01:45:38 -0500

    store screenshots, extract link and text

---
+++ Trace/BrowserIntegration.swift
@@ -0,0 +1,8 @@
+//
+//  BrowserIntegration.swift
+//  Trace
+//
+//  Created by Elliot Anderson on 1/19/26.
+//
+
+import Foundation
---
+++ Trace/GalleryView.swift
@@ -0,0 +1,8 @@
+//
+//  GalleryView.swift
+//  Trace
+//
+//  Created by Elliot Anderson on 1/19/26.
+//
+
+import Foundation
